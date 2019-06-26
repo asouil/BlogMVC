eb1fcb2c82ff703d1dfbfaf4f41f14358fae99a5
asouil <amaella.souil@gmail.com>
2019-06-26 12:31:52 +0200

    modifications d'éléments de la structure

--- www/src/Controller/UsersController.php
+++ www/src/Controller/UsersController.php
@@ -19,10 +19,89 @@ class UsersController extends Controller
             $this->users,
             $this->generateUrl('users')
         );
-        
+
         //on va voir pour définir sur quelle page on envoi notre utilisateur avant de le diriger sur la page...
         // là je me renseigne sur les variables qu'on peut envoyer à twig
         if($users){
+            if(!empty($_POST)){
+                //verif pour modif mdp
+                if(	isset($_POST["passwordOld"]) && !empty($_POST["passwordOld"]) &&
+                    isset($_POST["password"]) && !empty($_POST["password"]) &&
+                    isset($_POST["passwordVerify"]) && !empty($_POST["passwordVerify"]) &&
+                    isset($_POST["robot"]) && empty($_POST["robot"])//protection robot
+                ){
+                    if(userConnect($user["mail"], $_POST["passwordOld"], true)){
+                        if ($_POST["password"] == $_POST["passwordVerify"]) {
+                            $password = password_hash(htmlspecialchars($_POST["password"]), PASSWORD_BCRYPT);
+                            $sql = "UPDATE `users` SET `password`=:password WHERE `id_user`=:id_user";
+                            $pdo = getDB($dbuser, $dbpassword, $dbhost,$dbname);
+                            $statement = $pdo->prepare($sql);
+                            $statement->execute([
+                                ":password" => $password,
+                                ":id_user" 	=> $user["id_user"]
+                            ]);
+                            //message modif ok
+                            $_SESSION['success'] = 'Votre mot de passe a bien été modifié';
+                        }else{
+                            //mdp correspondent pas
+                            $_SESSION['error'] = 'Les deux mots de passes ne correspondent pas.';
+                        }
+                    }else{
+                        //erreur 
+                        $_SESSION['error'] = 'Mot de passe incorrect';
+                    }
+                }
+                elseif(isset($_POST["lastname"]) && !empty($_POST["lastname"]) &&
+                    isset($_POST["firstname"]) && !empty($_POST["firstname"]) &&
+                    isset($_POST["address"]) && !empty($_POST["address"]) &&
+                    isset($_POST["robot"]) && empty($_POST["robot"])){
+
+                        if(isset($_POST["lastname"]) && !empty($_POST["lastname"]) &&
+                            isset($_POST["firstname"]) && !empty($_POST["firstname"]) &&
+                            isset($_POST["address"]) && !empty($_POST["address"]) &&
+                            isset($_POST["zipCode"]) && !empty($_POST["zipCode"]) &&
+                            isset($_POST["city"]) && !empty($_POST["city"]) &&
+                            isset($_POST["country"]) && !empty($_POST["country"]) &&
+                            isset($_POST["phone"]) && !empty($_POST["phone"]) &&
+                            isset($_POST["robot"]) && empty($_POST["robot"])
+                        ){
+                            $sql = 'SELECT * FROM users WHERE id_user = ?';
+                            $pdo = getDB($dbuser, $dbpassword, $dbhost,$dbname);
+                            $statement = $pdo->prepare($sql);
+                            $statement->execute([htmlspecialchars($_POST['id_user'])]);
+                            $user = $statement->fetch();
+
+                            if($user) {
+                                $sqlparts = []; //:Array
+                                $fields = []; //:Array
+                                foreach($_POST as $key => $userInfo) {
+                                    if($key != 'robot' && $key != 'id_user') {
+
+                                        //On push "$key = ?" dans array $sqlparts
+                                        $sqlparts[] = $key.' = ?';
+
+                                        //On push la valeur de $userInfo dans $fields
+                                        $fields[] = $userInfo;
+                                    }
+                                }
+
+                                //On push l'id de l'utilisateur en dernier
+                                $fields[] = $_POST['id_user'];
+
+                                //On convertit le tableau $sqlparts en String en séparant ses cases par des virgules ',' 
+                                $sqlparts = implode(',', $sqlparts);
+
+                                //UPDATE users SET lastname = ?,firstname = ?,address = ?,zipCode = ?,city = ?,country = ?,phone = ? WHERE id_user = ?
+                                $sql = "UPDATE users SET ".$sqlparts.' WHERE id_user = ?';
+                                $req = $pdo->prepare($sql);
+                                $req->execute($fields);
+                            }
+                            
+                            }else{//fin verif user existe
+                                userConnect($_POST["mail"], $_POST["password"]);
+                            }
+                        }
+                }
             $title = "Connexion";
             $this->render(
                 "user/login",
@@ -43,18 +122,19 @@ class UsersController extends Controller
                 ]
             );
         }
-
-
-
-
     }
 
     public function show(){
         $user = $this->user->find($id);
         if (!$user) {
-            throw new \Exception('Aucun article ne correspond à cet ID');
+            throw new \Exception('Aucun utilisateur ne correspond à cet ID');
         }
-
+        if($user->getConnect()){
+                echo 	'<h1>Profil</h1>';
+                //tableau des commandes
+                foreach ($orders as $order) {
+                    echo '<a href="'.uri("confirmationDeCommande.php?id=").$order["id"].'">commande n°'.$order["id"].'- '.number_format($order["priceTTC"], 2, ',' ,'.').'€ </a><br />';
+                }
         if ($user->getId() !== $user[$id]) {
             $url = $this->generateUrl('user', [
                     'id' => $id, 
@@ -64,16 +144,14 @@ class UsersController extends Controller
             header('Location: ' . $url);
             exit();
         }
-
-        /*$order = $this->order->allInId($post->getId());
-        $title = "article : " . $post->getName();
         $this->render(
-            "post/show",
+            "user/profil",
             [
                 "title" => $title,
-                "order" => $order,
-                "post" => $post
+                "user" => $user,
+                "paginate" => $paginatedQuery->getNavHTML()
             ]
-        );*/
+        );
     }
 }
+
---
+++ www/views/cautions/mentions.php
@@ -0,0 +1,19 @@
+Conditions générales de vente (CGV) : 
+prix (les prix sont exprimés en euros et TTC), 
+frais et date de livraison, 
+modalités de paiement, service après-vente, droit de rétractation, durée de l'offre, 
+coût de la technique de communication à distance ;
+
+Le règlement général sur la protection des données (RGPD) ajoute des obligations liées à la transparence de l'information, spécialement pour les fichiers et traitement informatiques. Les données suivantes doivent être mentionnées :
+
+coordonnées du délégué à la protection des données (DPO ou DPD) de l'organisme, s'il a été désigné, ou d'un point de contact sur les questions de protection des données personnelles ;
+finalité poursuivie par le traitement auquel les données sont destinées ;
+caractère obligatoire ou facultatif des réponses et conséquences éventuelles à l'égard de l'internaute d'un défaut de réponse ;
+destinataires ou catégories de destinataires des données ;
+droits d'opposition, d'interrogation, d'accès et de rectification ;
+le cas échéant, les transferts de données à caractère personnel envisagés à destination d'un Etat n'appartenant pas à l'Union européenne.
+base juridique du traitement de données (c'est-à-dire ce qui autorise légalement le traitement : il peut s'agir du consentement des personnes concernées, du respect d'une obligation prévue par un texte, de l'exécution d'un contrat notamment) ;
+mention du droit d'introduire une réclamation (plainte) auprès de la CNIL.
+
+
+<!--Ce site est destiné uniquement à but d'appretissage -->
